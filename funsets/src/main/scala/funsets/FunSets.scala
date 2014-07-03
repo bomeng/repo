8984fd05455e866bf40c12c3f1eb83f7e6eb1fae
Bo Meng <mengbo@hotmail.com>
2014-07-02 17:22:25 -0700

    fix bugs

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -55,8 +55,8 @@ object FunSets {
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (a > bound) false
-      else if (contains(s, a) && p(a)) true
+      if (a > bound) true
+      else if (s(a) && !p(a)) false
       else iter(a + 1)
     }
     iter(-bound)
